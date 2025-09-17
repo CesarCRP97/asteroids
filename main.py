c4ad1ff6b58f685b6439143d36d64ee473e71252
CesarCRP97 <ccrp97@hotmail.es>
2025-09-17 13:44:08 +0200

    solved rotate & update extended

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
                 return
         screen.fill("black")
 
-        player.update()
+        player.update(dt)
         player.draw(screen)
 
         pygame.display.flip()
